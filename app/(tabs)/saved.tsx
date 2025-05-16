ecd9b012a864f3cf967d35c069c8d35d551789e8
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-05-16 04:43:45 +0000

    saved page

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const saved = () => {
+  return (
+    <View>
+      <Text>saved</Text>
+    </View>
+  );
+};
+
+export default saved;
+
