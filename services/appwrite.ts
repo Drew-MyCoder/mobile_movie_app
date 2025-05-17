12e1092ffca095d901563d32b5c39bb36029f55d
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-05-17 06:02:10 +0000

    fetchmovie detail

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,5 +1,6 @@
 // track the searches made by a user
 import { Client, Databases, ID, Query } from "react-native-appwrite";
+import { TMDB_CONFIG } from "./api";
 
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
@@ -64,5 +65,22 @@ export const getTrendingMovies = async (): Promise<TrendingMovie[] | undefined> 
     } catch (error) {
         console.log(error);
         return undefined;
+    }
+}
+
+export const fetchMovieDetails = async (movieId: string): Promise<MovieDetails>  => {
+    try {
+        const response = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,{
+            method: 'GET',
+            headers: TMDB_CONFIG.header,
+        });
+
+        if(!response.ok) throw new Error('Failed to fetch movie details');
+
+        const data = await response.json();
+        return data;
+    } catch(error){
+        console.log(error);
+        throw error;
     }
 }
